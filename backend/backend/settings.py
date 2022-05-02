c1029442d7df81698fe7b607ab91ac15a39f3822
Yi Lyu <marcolyu>
2022-05-01 20:36:04 -0500

    added todo functions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,10 @@
 from flask import Flask
-import sqlalchemy
+import pandas as pd
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_pyfile("instance.cfg")
 
+df = pd.read_csv("instance/data.csv", encoding="latin-1")
 
+tweets = df.iloc[:, -1].tolist()
--- backend/backend/views/main_view.py
+++ backend/backend/views/main_view.py
@@ -1,9 +1,48 @@
 from flask import Blueprint, request, session, jsonify
+from backend.settings import tweets
 
 main = Blueprint("main", __name__)
+
+def rank(documents, query):
+    """
+    TODO: complte this function
+    """
+    return documents
+
+def get_sentiment(sentence):
+    """
+    TODO: return the sentiment of the sentiment
+    """
+    sentiment = None
+    return sentiment
 
 @main.route("/", methods=["GET", "POST"])
 def home():
     if (request.method == "GET"):
-        response = "Hello World"
+        response = tweets[5]
         return jsonify({"data": response})
+
+@main.route("/query", methods=["GET"])
+def query():
+    q = request.json.get("query")
+    start = request.json.get("from")
+    end = request.json.get("to")
+    if start == None:
+        start = 0
+    if end == None:
+        end = 10
+    reordered_tweets = rank(tweets, q)
+
+    res = []
+    for tweet in reordered_tweets[start:end]:
+        obj = {
+            "text": tweet,
+            "sentiment": get_sentiment(tweet)
+        }
+        res.append(obj)
+
+    return jsonify({"data": res})
+
+@main.route("/get_all_data")
+def get_all_tweets():
+    return jsonify({"data": tweets})
